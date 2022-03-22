bb353caf28fcb7fda93da05fee700a90b9ddf6f6
ravikishorereddy <ravikishorereddy309@gmail.com>
2022-03-22 18:58:21 +0530

    Edit Details of Contact

--- AddressBookProblems/Program.cs
+++ AddressBookProblems/Program.cs
@@ -64,6 +64,35 @@ namespace AddressBookProblems
             contactDetailsList.Add(contactDetails);
             contactDetailsMap.Add(FirstName, contactDetails);
         }
+        public void EditDetails(string key)
+        {
+            if (contactDetailsMap.ContainsKey(key))
+            {
+                Console.WriteLine("Enter Your First name");
+                string FirstName = Console.ReadLine();
+                Console.WriteLine("Enter Your Last name");
+                string LastName = Console.ReadLine();
+                Console.WriteLine("Enter Your Address");
+                string Address = Console.ReadLine();
+                Console.WriteLine("Enter Your City");
+                string City = Console.ReadLine();
+                Console.WriteLine("Enter Your State");
+                string State = Console.ReadLine();
+                Console.WriteLine("Enter Your Zipcode");
+                int Zipcode = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter Your Phone number");
+                int PhoneNumber = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter Your Email Id");
+                string EmailId = Console.ReadLine();
+                ContactDetails contactDetails = new ContactDetails(FirstName, LastName, Address, City, State, Zipcode, PhoneNumber, EmailId);
+                contactDetailsList.Add(contactDetails);
+                contactDetailsMap[key] = contactDetails;
+            }
+            else
+            {
+                Console.WriteLine("Key not found");
+            }
+        }
         public void ComputeDetails()
         {
             foreach (ContactDetails contact in contactDetailsList)
@@ -81,7 +110,7 @@ namespace AddressBookProblems
             {
                 Console.WriteLine("1 : To Add a Contact Details");
                 Console.WriteLine("2: To Compute Details");
-
+                Console.WriteLine("3: To Edit Details");
                 try
                 {
                     option = int.Parse(Console.ReadLine());
@@ -92,6 +121,11 @@ namespace AddressBookProblems
                             break;
                         case 2:
                             details.ComputeDetails();
+                            break;
+                        case 3:
+                            Console.WriteLine("Enter a First Name to Edit");
+                            string key = Console.ReadLine();
+                            details.EditDetails(key);
                             break;
                         default:
                             Console.WriteLine("Wrong key");
